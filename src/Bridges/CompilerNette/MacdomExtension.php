4dbb514df1eba611818ae72837dce3dc01d91eb4
Machy8 <8machy@seznam.cz>
2017-09-23 15:06:14 +0200

    - Added Nette bridges

---
+++ src/Bridges/CompilerNette/MacdomExtension.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ *
+ * Copyright (c) Vladimír Macháček
+ *
+ * For the full copyright and license information, please view the file license.md
+ * that was distributed with this source code.
+ *
+ */
+
+declare(strict_types = 1);
+
+namespace GoogleClosureCompiler\Bridges\CompilerNette;
+
+use Nette\DI\CompilerExtension;
+
+
+class GoogleClosureCompilerExtension extends CompilerExtension
+{
+
+	public function loadConfiguration()
+	{
+		$builder = $this->getContainerBuilder();
+		$builder->addDefinition($this->prefix('compiler'))->setClass('GoogleClosureCompiler\Compiler');
+	}
+
+}
